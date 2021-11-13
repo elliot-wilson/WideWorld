f4c2135a13a3b1b1b762a12850733b6f91f2c43a
Elliot <elliotandrewwilson@gmail.com>
2021-11-13 13:03:27 -0500

    add scroll to splash locations box

--- frontend/components/homepage/splash_locations_box.jsx
+++ frontend/components/homepage/splash_locations_box.jsx
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom';
 class SplashLocationsBox extends React.Component {
     
     componentDidMount() {
+        window.scrollTo(0, 0);
         this.props.fetchRandomLocations();
     }
 

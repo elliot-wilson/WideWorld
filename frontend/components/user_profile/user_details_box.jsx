79c5848b7cf368538facc1efe84a274f5c9ffab4
Elliot Wilson <87868033+elliot-wilson@users.noreply.github.com>
2021-11-22 19:48:59 -0500

    added image meta info

--- frontend/components/user_profile/user_details_box.jsx
+++ frontend/components/user_profile/user_details_box.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle, faUserAlt } from '@fortawesome/free-solid-svg-icons';
+import { faUserAlt } from '@fortawesome/free-solid-svg-icons';
 
 const UserDetailsBox = (props) => {
 
